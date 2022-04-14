5ac8f99749ef23150cdf8ba1eba1fea889fb48be
solovev.a <solovev.a@skbkontur>
2022-04-14 19:43:49 +0500

    добавлена проверка UDP портов

--- scannerTCPandUPD/main.py
+++ scannerTCPandUPD/main.py
@@ -1,24 +1,49 @@
 import socket
 import threading
 
+print_lock = threading.Lock()
 
-def scan_port(ip, port):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def scan_port(ip, port, type_protocol):
+    sock = None
+
+    if type_protocol == "TCP":
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    elif type_protocol == "UDP":
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
     sock.settimeout(0.5)
     try:
         connect = sock.connect((ip, port))
-        print("Port:", port, "it is open")
+
+        print_lock.acquire()
+        print(type_protocol, " Port:", port, "it is open")
+        print_lock.release()
+
         sock.close()
-    except Exception as e:
+    except TimeoutError as e:
+        pass
+    except OSError as e:
         pass
 
 
-def scan_ports(ip, range_port_nums):
+def scan_tcp_ports(ip, range_port_nums):
     for i in range_port_nums:
-        thread = threading.Thread(target=scan_port, args=(ip, i))
+        thread = threading.Thread(target=scan_port, args=(ip, i, "TCP"))
         thread.start()
 
 
+def scan_udp_ports(ip, range_port_nums):
+    for i in range_port_nums:
+        thread = threading.Thread(target=scan_port, args=(ip, i, "UDP"))
+        thread.start()
+
+
+def start_port_scanner(ip, range_port_nums):
+    scan_tcp_ports(ip, range_port_nums)
+    scan_udp_ports(ip, range_port_nums)
+
+
 if __name__ == '__main__':
-    scan_ports("127.0.0.1", range(500))
+    start_port_scanner("127.0.0.1", range(1000))
 
